2c163b45d5691037c21f67242f899d8555efb877
AndrewVitrenko <57104862+AndrewVitrenko@users.noreply.github.com>
2020-05-13 23:41:23 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -11,42 +11,26 @@ $(document).ready(function() {
     })
 })    
 
-//let left = $('.code').css('left')
-
-/*$(window).scroll(function() {
-
-    
-    
+let lastScrollPosition = 0
+const left = parseFloat($('.code').css('left'))
+console.log (left)
 
 
-    if (wt + wh >= et) {
-        if (left == '15%') {
-            
-        }
-        else if (left == '7%') {
-            $('.code').css({left: '0'})
-        }
-    }
-    else if (wt + wh <= et + eh) {
-        $('.code').css({left: left})
-    }
-})*/
-
-$(window).bind('mousewheel', function(event) {
+$(window).scroll(function() {
     const et = $('.code').offset().top;
     const eh = $('.code').outerHeight();
     const wt = $(window).scrollTop();
     const wh = $(window).height();
-    let left = $('.code').css('left')
 
-    if (event.originalEvent.wheelDelta <= 0) {
-        if (wt + wh >= et) {
-            $('.code').css({left: left/2})
-        }
+    if (wt > lastScrollPosition && wt + wh >= et) {
+        const animatedleft = left/2
+        console.log('down', animatedleft)
+        $('.code').css('left', animatedleft)
     }
-    else if (event.originalEvent.wheelDelta >= 0) {
-        if (wt + wh <= et + eh) {
-            $('.code').css({left: left})
-        }
+    else if (wt < lastScrollPosition && wt + wh <= et + eh) {
+        console.log('up', left)
+        $('.code').css('left', left)
     }
+
+    lastScrollPosition = wt
 })
